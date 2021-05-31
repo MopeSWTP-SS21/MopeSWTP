fd39eea428f208382ea30276d2c0e76f602d35a1
manuEbg <m.waechtershaeuser@web.de>
2021-05-31 21:17:48 +0200

    Initialize Log4J-Logger to Fix #38

--- src/main/java/Server/MopeLSPServerLauncher.java
+++ src/main/java/Server/MopeLSPServerLauncher.java
@@ -1,11 +1,15 @@
 package Server;
 
 import Client.MopeLSPClient;
+import org.apache.log4j.LogManager;
 import org.eclipse.lsp4j.jsonrpc.Launcher;
 import org.eclipse.lsp4j.launch.LSPLauncher;
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.eclipse.lsp4j.services.LanguageServer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.impl.Log4jLoggerAdapter;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -24,10 +28,17 @@ public class MopeLSPServerLauncher {
     private static String host;
     private static int port;
     private static Future<Void> serverListening;
+    private static Logger logger = LoggerFactory.getLogger(MopeLSPServerLauncher.class);
 
     private static Future<Void> LaunchServer() throws IOException {
+
+        System.setProperty(Log4jLoggerAdapter.ROOT_LOGGER_NAME, "TRACE");
+        //Logger logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+
+
         server = new MopeLSPServer();
         serverSocket = new ServerSocket(port);
+        logger.info("Server socket Listeng");
         System.out.println("Server socket listening");
         System.out.flush();
         socket = serverSocket.accept();
