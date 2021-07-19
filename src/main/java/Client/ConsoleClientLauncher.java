e0cd2b2d55e0d8cd914f50fbfa1adb76da8017e1
manuEbg <m.waechtershaeuser@web.de>
2021-07-19 19:34:47 +0200

    separated ConsoleMenu

--- src/main/java/Client/ConsoleClientLauncher.java
+++ src/main/java/Client/ConsoleClientLauncher.java
@@ -27,6 +27,7 @@ public class ConsoleClientLauncher {
     private static String host;
     private static int port;
     private static Scanner sc = new Scanner(System.in);
+    private static ConsoleMenu menu;
     private static final Logger logger = LoggerFactory.getLogger(ConsoleClientLauncher.class);
     private static Future<Void> clientListening;
 
@@ -35,6 +36,7 @@ public class ConsoleClientLauncher {
         this.port = port;
         client = new MopeLSPClient();
         socket = new Socket(host, port);
+        menu = new ConsoleMenu(client);
     }
 
     public Future<Void> LaunchClient() throws IOException {
@@ -71,101 +73,9 @@ public class ConsoleClientLauncher {
         ConsoleClientLauncher launcher = new ConsoleClientLauncher(host, port);
 
         clientListening = launcher.LaunchClient();
-
-        ConsoleMenue();
-
-
+        menu.run();
         StopClient();
 
-    }
-
-    private static void ConsoleMenue(){
-        boolean running=true;
-
-        String[] menuItems = new String[] {
-                "1: Initialize server",
-                "2: Get compiler version",
-                "3: Load File",
-                "4: Load model",
-                "5: Check Model",
-                "6: Initialize Model",
-                "7: Add Folder to ModelicaPath",
-                "8: Show ModelicaPath",
-                "9 : Exit",
-                "10 : Complete",
-                "11 : Get Documentation",
-                "12 : send Expression"
-        };
-
-        while(running)
-        {
-
-            for (String item: menuItems ) {
-                System.out.println(item);
-            }
-            System.out.print(">");
-
-            int command= sc.nextInt();
-            switch(command){
-                case 1:
-                    client.initServer();
-                    break;
-                case 2:
-                    System.out.println(client.compilerVersion());
-                    break;
-                case 3:
-                    System.out.print("path: ");
-                    String filePath = sc.next();
-                    System.out.println(client.loadFile(filePath));
-                    break;
-                case 5:
-                    System.out.print("modelName: ");
-                    String name = sc.next();
-                    System.out.println(client.checkModel(name));
-                    break;
-                case 6:
-                    System.out.println("not implemented");
-                    break;
-                case 9:
-                    running=false;
-                    break;
-                case 7:
-                    System.out.print("path: ");
-                    String path = sc.next();
-                    System.out.println(client.addPath(path));
-                    break;
-                case 4:
-                    System.out.print("modelName: ");
-                    String loadName = sc.next();
-                    System.out.println(client.loadModel(loadName));
-                    break;
-                case 8:
-                    System.out.println(client.modelicaPath());
-                    break;
-                case 10:
-                    System.out.print("File: ");
-                    String compFile = sc.next();
-                    System.out.print("Line: ");
-                    int line = sc.nextInt();
-                    System.out.print("Column: ");
-                    int col = sc.nextInt();
-                    System.out.println(client.complete(compFile, line, col));
-                    break;
-                case 11:
-                    System.out.print("className: ");
-                    String docName = sc.next();
-                    System.out.println(client.getDocumentation(docName));
-                    break;
-                case 12:
-                    System.out.print("expression: ");
-                    String modelicaCommand = sc.next();
-                    System.out.println(client.executeCommand(modelicaCommand));
-                    break;
-                default:
-                    logger.info("wrong entry");
-                    break;
-            }
-        }
     }
 
 }
